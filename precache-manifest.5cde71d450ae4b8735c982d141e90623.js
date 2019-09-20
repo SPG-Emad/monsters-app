d2319acd576f408099f9c4bc302fae8a7bb2046a
Emad khan <Spidergrey.emad@gmail.com>
2019-09-20 15:13:56 +0500

    Updates

---
+++ precache-manifest.5cde71d450ae4b8735c982d141e90623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419afa9027fafab449c30130a6ad4964",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "a529867fa9f5ca6ef476",
+    "url": "/monsters-app/static/css/main.e5aaf934.chunk.css"
+  },
+  {
+    "revision": "842745e26725eb7f74a7",
+    "url": "/monsters-app/static/js/2.9a4ddc69.chunk.js"
+  },
+  {
+    "revision": "a529867fa9f5ca6ef476",
+    "url": "/monsters-app/static/js/main.f75a3c42.chunk.js"
+  },
+  {
+    "revision": "c116df715470bebc3d6c",
+    "url": "/monsters-app/static/js/runtime~main.b672ca14.js"
+  }
+]);
